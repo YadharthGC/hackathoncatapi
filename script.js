1a9d5b4c436cfa7ad8d36b7e920512de75f66221
YadharthGC <85919766+YadharthGC@users.noreply.github.com>
2021-07-22 18:58:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+fetch("https://cataas.com/api/cats")
+    .then(data => data.json())
+    .then(cats => load(cats));
+
+function load(cats) {
+    var catlist = document.createElement("div");
+    catlist.className = "container"
+    catlist.id = "catlist"
+    cats.forEach((cat) => {
+        var a = document.createElement('div');
+        a.innerHTML = `
+    <img src="https://cataas.com/cat/${cat.id}" style="height:100px;width:100px;object-fit:cover"></img><br><br>
+    <div style="text-align:left;color:gray">Created on:${new Date(cat.created_at).toDateString()}</div>
+               <div style="text-align:left;color:gray">Tags:${cat.tags}</div>
+            </div>
+    `
+        catlist.append(a);
+    })
+    document.body.append(catlist);
+}
+
 function search() {
+    document.querySelector("#catlist").remove();
     const s = document.querySelector(".x").value;
     console.log("searching");
 
@@ -16,7 +38,8 @@ function search() {
             var a = document.createElement("div");
             a.id = 'cat';
             a.innerHTML = `
-            <div class="popup" onclick="myFunction()"><input type="image" class="popup" id="img" src="https://cataas.com/cat/${cat.id}"> <span class="popuptext" id="myPopup"><img src="https://cataas.com/cat/${cat.id}" style="width:150px;height:150px;"></span><div>
+            <div class="popup" onclick="myFunction()"><input type="image" class="popup" id="img" src="https://cataas.com/cat/${cat.id}"> 
+            <span class="popuptext" id="myPopup"><img src="https://cataas.com/cat/${cat.id}" style="width:150px;height:150px;"></span><div>
             <div>
                <div>Created on:${new Date(cat.created_at).toDateString()}</div>
                <div>Tags:${cat.tags}</div>
@@ -28,7 +51,7 @@ function search() {
         })
         document.body.append(catlist);
     }
-    document.querySelector("#catlist").remove();
+
 }
 
 function myFunction() {
